7d18b84357ea2f0476e0c87d2124e76e6ac36b64
erik-ekberg-sg <erik@singlegrain.com>
2021-08-02 15:41:32 -0700

    Breakup lines

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 data "local_file" "this" { filename = var.filename }
 locals {
-        variables = merge([for list in [for s in split(var.newline_delimiter, data.local_file.this.content): split(var.inline_delimiter, s)]: length(list) >= 2 ? { (list[0]) = join(var.inline_delimiter, slice(list, 1, length(list))) } : {}]...)
+        variables = merge([
+		for list in [
+			for s in split(var.newline_delimiter, data.local_file.this.content): split(var.inline_delimiter, s)
+		]: length(list) >= 2 ? { (list[0]) = join(var.inline_delimiter, slice(list, 1, length(list))) } : {}
+	]...)
 }
 
